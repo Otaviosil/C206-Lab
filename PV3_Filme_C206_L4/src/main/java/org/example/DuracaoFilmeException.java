79a48c7166223f3036a219287635de4615a92430
Otavio Silva <133617815+Otaviosil@users.noreply.github.com>
2023-11-07 14:03:40 -0300

    Delete PV3_Filme_C206_L4 directory

--- PV3_Filme_C206_L4/src/main/java/org/example/DuracaoFilmeException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class DuracaoFilmeException extends Exception {
-    public DuracaoFilmeException() {
-        System.out.println("Não é permitido cadastrar veículos com ano de lançamento nulo, ou menor que 0");
-    }
-}
